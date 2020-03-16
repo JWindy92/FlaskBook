0751e6cfc08e49babf1c0dcd36282da01a23bbda
John Widmaier <johnwid92@gmail.com>
2020-03-16 16:23:30 -0400

    up to Ch11 chkout 11d pagin widget

--- app/models.py
+++ app/models.py
@@ -87,6 +87,7 @@ class User(UserMixin, db.Model):
     avatar_hash = db.Column(db.String(32))
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -200,3 +201,10 @@ class AnonymousUser(AnonymousUserMixin):
         return False
 
 login_manager.anonymous_user = AnonymousUser
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
